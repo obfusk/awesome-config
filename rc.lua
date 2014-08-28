760040c4bc41baadd0d5df74b9a5b212e9c162a5
Felix C. Stegerman <flx@obfusk.net>
2014-08-29 00:38:00 +0200

    make Pinentry floating

--- rc.lua
+++ rc.lua
@@ -340,6 +340,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "pinentry" },
       properties = { floating = true } },
+    { rule = { class = "Pinentry" },
+      properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
